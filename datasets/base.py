bcd093292f72c3c0485336ee197133452413114d
Leilani Battle <leibatt@gmail.com>
2013-07-09 11:08:52 -0400

    added a datasets stub

---
+++ datasets/base.py
@@ -0,0 +1,19 @@
+class BaseDataset:
+  """
+  Base
+  """
+
+  def __init__(self, name=""):
+    """
+    Args
+      name: name of this (set) of features
+    """
+    self.name = name
+
+  def getVisualizaations(self):
+    """
+    return an array of visualization objects
+    """
+    None
+
+
---
+++ datasets/random.py
@@ -0,0 +1,19 @@
+from sklearn.datasets import make_classification
+from base import BaseDataset
+import numpy as np
+
+class RandomDataset(BaseDataset):
+  def __init__(self):
+    BaseDataset.__init__(self, "random")
+
+  def getVisualizations(self):
+    self.xs = []
+    self.ys = []
+    numsamples = 100
+    visualizations = []
+    for i in range(numrows):
+      d = make_classification(n_samples=100)
+      self.xs.append(d[0])
+      self.ys.append(d[1])
+      visualizations.append(d[0])
+    return visualizations
