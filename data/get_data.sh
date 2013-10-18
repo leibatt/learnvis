77593959a61bf0ff6e9bdc2e029423721e3c50a3
eugene wu <sirrice@gmail.com>
2013-10-18 14:11:40 -0400

    updated get_data to extract ggplot2 data into data_sets/ggplot2

--- data/get_data.sh
+++ data/get_data.sh
@@ -2,6 +2,11 @@
 
 scp -r learnvis@hammer.csail.mit.edu:~/data_sets .
 
-
 scp -r learnvis@hammer.csail.mit.edu:~/*.zip .
 unzip manyeyes_crawler.zip
+
+
+mkdir -p ./data_sets/ggplot2
+cd ./data_sets/ggplot2
+tar -xf ../../../scrapers/ggplot2/ggplotdata.tar.gz
+cd -
